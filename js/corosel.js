b35640ddb48308edf6f6474dfcbf8d60a149903b
Mosh <qwwerrrty11@gmail.com>
2022-04-16 18:07:04 +0530

    product detil list updated

---
+++ js/corosel.js
@@ -0,0 +1,18 @@
+var proimg = document.getElementById("ProductImg");
+var smimg = document.getElementsByClassName("simg");
+
+smimg[0].onclick = function () {
+  proimg.src = smimg[0].src;
+};
+smimg[1].onclick = function () {
+  proimg.src = smimg[1].src;
+};
+smimg[2].onclick = function () {
+  proimg.src = smimg[2].src;
+};
+smimg[3].onclick = function () {
+  proimg.src = smimg[3].src;
+};
+smimg[4].onclick = function () {
+  proimg.src = smimg[4].src;
+};
